82a99148e1b3dd566cfe9d92cf5cdc0e5ec0650c
yanglixin <yanglixin@qiniu.com>
2023-06-21 09:45:28 +0800

    first commit

---
+++ layouts/UploadLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="bg-[#F8F8F8] h-[100vh]"  
+  >
+  <TopNav />
+    <div>
+      <SideNavMain />
+    </div>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+const route = useRoute()
+</script>
